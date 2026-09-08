b6089ff00d73e07d9dde881371cf14876318193f
Olaifa Promise <olaifapromise1@gmail.com>
2026-09-08 14:57:26 +0100

    Add the help video tutorials page

---
+++ src/app/help/video-tutorials/page.tsx
@@ -0,0 +1,10 @@
+import { TutorialLibrary } from "@/components/help/TutorialLibrary";
+
+export const metadata = {
+  title: "Video tutorials",
+  description: "Short, practical walkthroughs for setting up, reviewing and launching Concierge.",
+};
+
+export default function VideoTutorialsPage() {
+  return <TutorialLibrary />;
+}
