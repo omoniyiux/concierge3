095883f7495d7960dbdca545cdedee0cb9e63440
Olaifa Promise <olaifapromise1@gmail.com>
2026-09-08 09:34:38 +0100

    Add the help docs page

---
+++ src/app/help/docs/page.tsx
@@ -0,0 +1,11 @@
+import { DocsIndex } from "@/components/help/DocsIndex";
+
+export const metadata = {
+  title: "Documentation",
+  description:
+    "Guides for setting up Concierge, publishing Pages, connecting the response path and helping visitors get the right answer.",
+};
+
+export default function DocsPage() {
+  return <DocsIndex />;
+}
